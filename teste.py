c62c596f3beccac71b6ceb6f52f73a1c8fc7aa1c
Pietra Ataulo <pietraataulo@gmail.com>
2024-05-09 14:19:51 -0300

    Criei o índice

--- teste.py
+++ teste.py
@@ -1,9 +1,11 @@
-soma = 0
-qtd = -1
-for e in range(0,101,2):
-    soma += e
-    qtd += 1
-print(f"A média dos pares de 1 até 100 é: {soma/qtd}")
+while True:
+    sabor = input('Sabor:')
+    if sabor != 'cp' and sabor != 'ac':
+        continue
+    else:
+        break
+print('Certo')
+
 
 
 
--- teste3.py
+++
@@ -1,5 +0,0 @@
-for e in range(0, 101, 2):
-    e += 2
-    print(e)
-print(e)
-print(e/2)
