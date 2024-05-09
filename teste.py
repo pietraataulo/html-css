4ffa3ff07b29fefe4c5d78230c06d0bec5ae2696
Pietra Ataulo <pietraataulo@gmail.com>
2024-05-08 21:16:10 -0300

    ex002 media query

---
+++ teste.py
@@ -0,0 +1,9 @@
+soma = 0
+qtd = -1
+for e in range(0,101,2):
+    soma += e
+    qtd += 1
+print(f"A média dos pares de 1 até 100 é: {soma/qtd}")
+
+
+
---
+++ teste3.py
@@ -0,0 +1,5 @@
+for e in range(0, 101, 2):
+    e += 2
+    print(e)
+print(e)
+print(e/2)
