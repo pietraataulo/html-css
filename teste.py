b0f1f13d39c928099b577f88ce525166758b7234
Pietra Ataulo <pietraataulo@gmail.com>
2024-05-10 16:20:08 -0300

    .

--- teste.py
+++ teste.py
@@ -1,12 +1,55 @@
-continuar = input('Continuar? ')
-
+total = 0
 while True:
-    continuar = input('Continuar?')
-    if continuar == 'N':
-        break
-    elif continuar == 'S':
+    while True:
+        sabor = input('Digite o sabor (CP/AC): ')
+        if sabor == 'AC' or sabor == 'CP':
+            break
+        else:
+            print('Sabor inválido. Tente novamente')
+            continue
+    while True:
+        tamanho = input('Digite o tamanho (P/M/G): ')
+
+        if tamanho in ['P', 'M', 'G']:
+            break
+        else:
+            print('Tamanho inválido, tente novamente')
+            continue
+
+    # validação   
+    preco = 0        
+    if sabor == 'CP':
+
+        sabor = 'Cupuaçu'
+        if tamanho == 'P':
+            preco = 9
+            total += preco
+        elif tamanho == 'M':
+            preco = 14
+            total += preco
+        else:
+            preco = 18
+            total += preco
+    else:
+        sabor == 'Açaí'
+        if tamanho == 'P':
+            preco = 11
+            total += preco
+        elif tamanho == 'M':
+            preco = 16
+            total += preco
+        else:
+            preco: int = 20
+    print(f'Você selecionou um {sabor} e {tamanho}: R$ {preco:.2f}')
+
+    # continuar 
+    continuar = input('Deseja mais alguma coisa? (S/N): ')
+    if continuar == 'S':
         continue
-    
+    elif continuar == 'N':
+        break
+total = total + preco
+print(f'O valor total a ser pago: R$ {total:.2f}')
 
 
 
